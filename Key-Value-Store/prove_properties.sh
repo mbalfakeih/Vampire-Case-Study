73ac877646493a1f9ee04dc7df7870c0695c9963
Mohammed Balfakeih <M.H.M.Balfakeih@student.tudelft.nl>
2025-05-20 13:30:41 +0200

    Added time info to prove_properties.sh

--- Key-Value-Store/prove_properties.sh
+++ Key-Value-Store/prove_properties.sh
@@ -10,9 +10,15 @@ find "$d"/results/ -name "*.out" -type f -delete
 for i in $(find "$d" -name "*.p"); do
     f="${i//$d}"
     f="${f:1:-2}"
+    echo "Running on $f"
     #Run vampire on each of the files and save their output to a separate directory
     #See https://vprover.github.io/usage.html
     vampire --input_syntax tptp --mode casc -t 300 "$d/$f.p" > "$d/results/$f.out"
-    echo $(grep "SZS status" "$d/results/$f.out")
+    a=$(grep "SZS status" "$d/results/$f.out") 
+    echo $a 
+    if [[ $a == *"Theorem"* ]] then
+        echo $(grep "Success in time" "$d/results/$f.out")
+    fi
+    printf "\n"
 done
 echo "For more information on individual results, check the results directory"
