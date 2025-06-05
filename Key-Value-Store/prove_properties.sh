0aae3534e77c4ff266e0df00541341d7f0be84a8
Mohammed Balfakeih <M.H.M.Balfakeih@student.tudelft.nl>
2025-06-05 17:14:59 +0200

    Made prove_properties also able to be ran on .imp files

--- Key-Value-Store/prove_properties.sh
+++ Key-Value-Store/prove_properties.sh
@@ -5,15 +5,17 @@
 #Resetting
 d=$(git rev-parse --show-toplevel)/Key-Value-Store
 mkdir -p "$d"/results/theorems
+mkdir -p "$d"/results/impossible
 find "$d"/results/ -name "*.out" -type f -delete
 
-for i in $(find "$d" -name "*.p"); do
+run () {
+for i in $(find "$d" -name "*$1"); do
     f="${i//$d}"
-    f="${f:1:-2}"
+    f="${f:1:$2}"
     echo "Running on $f"
     #Run vampire on each of the files and save their output to a separate directory
     #See https://vprover.github.io/usage.html
-    vampire --input_syntax tptp --mode casc -t 300 "$d/$f.p" > "$d/results/$f.out"
+    vampire --input_syntax tptp --mode casc -t 300 "$d/$f$1" > "$d/results/$f.out"
     a=$(grep "SZS status" "$d/results/$f.out") 
     echo $a 
     if [[ $a == *"Theorem"* ]] then
@@ -21,4 +23,8 @@ for i in $(find "$d" -name "*.p"); do
     fi
     printf "\n"
 done
+}
+run ".p" "-2"
+# Uncomment the line below to also run it on impossible files
+# run ".imp" "-4"
 echo "For more information on individual results, check the results directory"
